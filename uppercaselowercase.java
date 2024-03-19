bc564144850beb2eb5a7acfe170b6da7d4d80286
SUHANA SHEREEF <159097906+Suhanashereef@users.noreply.github.com>
2024-03-19 15:09:50 +0530

    uppercaselowercase.java

---
+++ uppercaselowercase.java
@@ -0,0 +1,19 @@
+class ststring
+{
+    public static void main(String args[])
+    {
+        String s1 = "Good Morning";
+        System.out.println(s1);
+        System.out.println("To upper case: "+s1.toUpperCase());
+        System.out.println("To Lower case: "+s1.toLowerCase());
+    }
+}
+
+output
+------
+  C:\Users\mlm\.jdks\openjdk-21.0.2\bin\java.exe "-javaagent:C:\Users\mlm\AppData\Local\JetBrains\IntelliJ IDEA Community Edition 2023.3.3\lib\idea_rt.jar=65377:C:\Users\mlm\AppData\Local\JetBrains\IntelliJ IDEA Community Edition 2023.3.3\bin" -Dfile.encoding=UTF-8 -Dsun.stdout.encoding=UTF-8 -Dsun.stderr.encoding=UTF-8 -classpath C:\Users\mlm\IdeaProjects\add.java\out\production\add.java ststring
+Good Morning
+To upper case: GOOD MORNING
+To Lower case: good morning
+
+Process finished with exit code 0
