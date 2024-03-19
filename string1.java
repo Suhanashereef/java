ec765bd8f201754815b5434e2124d15a631365d1
SUHANA SHEREEF <159097906+Suhanashereef@users.noreply.github.com>
2024-03-19 15:07:43 +0530

    string1.java

---
+++ string1.java
@@ -0,0 +1,28 @@
+class string
+{
+    public static void main(String args[])
+    {
+        String str="HELLO";
+        String toCheckstr="HELLO";
+        if(str.startsWith(toCheckstr))
+        {
+            System.out.println("The string does start with "+toCheckstr);
+
+        }
+        else
+        {
+            System.out.println("The string does not start with "+toCheckstr);
+        }
+    }
+}
+
+output
+------
+  C:\Users\mlm\.jdks\openjdk-21.0.2\bin\java.exe "-javaagent:C:\Users\mlm\AppData\Local\JetBrains\IntelliJ IDEA Community Edition 2023.3.3\lib\idea_rt.jar=65285:C:\Users\mlm\AppData\Local\JetBrains\IntelliJ IDEA Community Edition 2023.3.3\bin" -Dfile.encoding=UTF-8 -Dsun.stdout.encoding=UTF-8 -Dsun.stderr.encoding=UTF-8 -classpath C:\Users\mlm\IdeaProjects\add.java\out\production\add.java String_1
+The string is java programming lab
+Length of the String is 20
+Character at second and fourth position is a,a
+The sub string using start index only is programming lab
+The sub string using start index only is java
+
+Process finished with exit code 0
